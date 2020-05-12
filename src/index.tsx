d6e6e7ba0b618404e8fe4ffb255aa5c52825f789
Jose <jeeturingan@gmail.com>
2020-05-12 15:45:13 +0800

    Added GCash API Integration

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './features/Payment/Payment';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
